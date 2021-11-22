9054587763c547603edb129efbd4aa6279e1d9be
hiagrawal <agrawal.hina13@gmail.com>
2021-11-22 17:23:27 +0530

    Hooks

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,7 +5,30 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
-describe('Auth Controller - Login', function(){
+describe('Auth Controller', function(){
+
+    before(function(done){
+        mongoose.connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/test-RESTAPI?retryWrites=true&w=majority')
+            .then(result => {
+                const user = new User({
+                    email: 'test@test.com',
+                    password: 'tester',
+                    name: 'Test',
+                    posts: [],
+                    _id: '5c0f66b979af55031b34728a'
+                });
+                return user.save();
+            })
+            .then(()=>{
+                done();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    })
+
+    beforeEach(function(){});
+    afterEach(function(){});
 
     it('should throw an error status 500 if accessing the database fails', function(done){
         sinon.stub(User, 'findOne');
@@ -30,53 +53,41 @@ describe('Auth Controller - Login', function(){
     });
 
     it('should send a response with a valid user status for an existing user', function(done){
-        mongoose.connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/test-RESTAPI?retryWrites=true&w=majority')
-            .then(result => {
-                const user = new User({
-                    email: 'test@test.com',
-                    password: 'tester',
-                    name: 'Test',
-                    posts: [],
-                    _id: '5c0f66b979af55031b34728a'
-                });
-                return user.save();
-            })
-            .then(()=>{
-                const req = {userId: '5c0f66b979af55031b34728a'};
-                const res = {
-                    statusCode: 500,
-                    userStatus: null,
-                    status: function(code){
-                        this.statusCode = code;
-                        return this; //this should be returned else it would fail as it will not be able to retrieve the value
-                    },
-                    json: function(data){
-                        this.userStatus = data.status;
-                    }
-                }
-                //Keep in mind.. we are able to use 'then' on calling method coz async await by default returns a promise.
-                //if we would have used promises for asynchronous calling instead of async await then we would not be able to use 'then'
-                AuthController.getStatus(req, res, () => {}).then(result => {
-                    expect(res.statusCode).to.be.equal(200);
-                    expect(res.userStatus).to.be.equal('I am new!');
+            
+        const req = {userId: '5c0f66b979af55031b34728a'};
+        const res = {
+            statusCode: 500,
+            userStatus: null,
+            status: function(code){
+                this.statusCode = code;
+                return this; //this should be returned else it would fail as it will not be able to retrieve the value
+            },
+            json: function(data){
+                this.userStatus = data.status;
+            }
+        }
+        //Keep in mind.. we are able to use 'then' on calling method coz async await by default returns a promise.
+        //if we would have used promises for asynchronous calling instead of async await then we would not be able to use 'then'
+        AuthController.getStatus(req, res, () => {}).then(result => {
+            expect(res.statusCode).to.be.equal(200);
+            expect(res.userStatus).to.be.equal('I am new!');
+            done();
+        });
 
-                //one thing is, once we run npm test, it does not quit the process automatically.
-                //and the reason for that is.. done detects if all processes are done executing and it finds that the mongodb is still running
-                //so once we are done executing our test cases, we disconnect to the db and then call 'done'
+    })
 
-                //also, once we are done executing, delete data from the database else it might be an issue like in our case if we run the test
-                //again, it throws an error as user with the id already exists
-                    User.deleteMany({}).then(() =>{
-                        mongoose.disconnect().then(()=>{
-                            done();
-                        })
-                    });  
-                });
-            })
-            .catch(err => {
-                console.log(err);
-            })
+    after(function(done){
+        //one thing is, once we run npm test, it does not quit the process automatically.
+        //and the reason for that is.. done detects if all processes are done executing and it finds that the mongodb is still running
+        //so once we are done executing our test cases, we disconnect to the db and then call 'done'
 
+        //also, once we are done executing, delete data from the database else it might be an issue like in our case if we run the test
+        //again, it throws an error as user with the id already exists
+        User.deleteMany({}).then(() =>{
+            mongoose.disconnect().then(()=>{
+                done();
+            })
+        });  
     })
 
 });
